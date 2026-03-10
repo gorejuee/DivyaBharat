483a16ef0ad20f5e5bce7a0a57fac784c9176eb6
Juee Gore <gorejuee08@gmail.com>
2026-03-10 23:06:26 +0530

    postgresql and sequelize connected

--- sacred-path-backend/server.js
+++ sacred-path-backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const sequelize = require('./config/database');
 require('dotenv').config()
 
 const app = express()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
@@ -11,6 +12,14 @@ app.use(express.json())
 app.get('/api/health', (req, res) => {
     res.json({ status: 'ok' })
 })
+
+sequelize.authenticate()
+    .then(() => {
+        console.log('DB connected successfully');
+    })
+    .catch((err) => {
+        console.error('DB connection failed:', err);
+    });
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
